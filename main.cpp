5170ebab5dfebb0cbb1beff001077cfb0751897a
ecyht2 <94816144+ecyht2@users.noreply.github.com>
2023-02-14 15:10:21 +0800

    Fixed grammar mistakes

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main()
 {
   cout<<"\tHi there player looks like you've just regained consciousness...\n"<<endl;
-  cout<<"\tYou are in the mansion of Agaroth, trapped in a game\n"<<endl;
-  cout<<"\tThere are nine rooms, six monsters and one Golden Chalice\n"<<endl;
-  cout<<"\tGet the Golden Chalice and return to the entrance to win"<<endl;
-  cout<<"\tType 'help' to get the list of commands and more details"<<endl;
+  cout<<"\tYou are in the mansion of Agaroth, trapped in a game.\n"<<endl;
+  cout<<"\tThere are nine rooms, six monsters and one Golden Chalice.\n"<<endl;
+  cout<<"\tGet the Golden Chalice and return to the entrance to win."<<endl;
+  cout<<"\tType 'help' to get the list of commands and more details."<<endl;
   cout<<"\nWelcome to Agaroth hope you enjoy the visit....\n"<<endl;
 
 
